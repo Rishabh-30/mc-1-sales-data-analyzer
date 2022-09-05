6603f53ff00b0c9245cc7d32ccbd2d96a0ebb128
Rishabh Pathak <rishabh.pathak30@hotmail.com>
2022-09-05 09:00:11 +0530

    completed with test run

--- src/main/java/com/jap/sales/TimeOnSiteComparator.java
+++ src/main/java/com/jap/sales/TimeOnSiteComparator.java
@@ -4,15 +4,15 @@ package com.jap.sales;
 import java.util.Comparator;
 
 public class TimeOnSiteComparator implements Comparator<SalesRecord> {
-    public int compare(SalesRecord salesRecord1 , SalesRecord salesRecord2){
-        if(salesRecord1.getTime_on_site() == salesRecord2.getTime_on_site()){
+    public int compare(SalesRecord salesRecord1, SalesRecord salesRecord2) {
+        if (salesRecord1.getTime_on_site() == salesRecord2.getTime_on_site()) {
             return 0;
         }
-        if(salesRecord1.getTime_on_site() > salesRecord2.getTime_on_site()){
+        if (salesRecord1.getTime_on_site() > salesRecord2.getTime_on_site()) {
             return -1;
-        }
-        else{
+        } else {
             return 1;
         }
     }
 }
+//
